9e95ae433deb36cd1cb4dc52972409eb8f845d8e
HundredPercentCoverage <bunglenutter@gmail.com>
2023-04-16 22:04:13 +0100

    First commit

---
+++ pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
+  </div>
+</template>
+
+<script setup>
+  definePageMeta({ layout: 'products' });
+  const { id } = useRoute().params;
+
+  const { data: product } = await useFetch(`https://fakestoreapi.com/products/${id}`);
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="product in products">
+        <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: 'products',
+  });
+
+  const { data: products } = await useFetch('https://fakestoreapi.com/products')
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+
+  p {
+    margin: 20px 0;
+  }
+</style>
